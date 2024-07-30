d96beb89c5d98e6e3a6143ddcf2312b72014d269
JunHo <sjrmd789@gmail.com>
2024-07-30 16:47:17 +0900

    3회차

--- 02.js
+++ 02.js
@@ -4,14 +4,20 @@
 const API_URL = "https://open.api.com/v1/data";
 const WRONG_URL = "https://open.api.com/v1/wrong";
 
-async function getData(url) {
+function getData(url) {
     // 3초 뒤 url 이 API_URL과 같다면 성공으로 처리를, 아닐 경우 실패로 처리를 하게 해주세요
-    let isUrl = await url === API_URL ? "성공" : "실패";
-
-    setTimeout(() => {
-        console.log(isUrl)
-    },3000)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => (url === API_URL ? resolve("성공") : reject("실패")), 3000);
+    });
 }
 
-getData(API_URL);
-getData(WRONG_URL);
+async function callGetData(url){
+    try {
+        const result = await getData(url);
+        console.log(result)
+    } catch(e){
+        console.log(e)
+    }
+}
+callGetData(API_URL)
+callGetData(WRONG_URL)
